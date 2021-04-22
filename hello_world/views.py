ed2719616950c606cccbf13fba8e5c0c07e47f0c
codewithkomal <komalljadhav2000@gmail.com>
2021-04-23 02:13:11 +0530

    Initial commit

---
+++ hello_world/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def homePageView(request):
+    return HttpResponse('Hello , I am Komal Laxman Jadhav from T1  batch  and  my PRN is 1841019')
